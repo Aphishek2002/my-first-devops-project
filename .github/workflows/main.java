600f85400d7c9a23e1dedfe9b861428566f48ddf
Aphishek2002 <130122877+Aphishek2002@users.noreply.github.com>
2026-04-18 17:32:59 +0530

    Replace workflow with Main class implementation
    Replaced GitHub Actions workflow with a Java class that prints a message.

--- .github/workflows/main.java
+++ .github/workflows/main.java
@@ -1,23 +1,5 @@
-name: Run My Java File
-
-on: push
-
-jobs:
-  run-java:
-    runs-on: ubuntu-latest
-
-    steps:
-      - name: Checkout code
-        uses: actions/checkout@v3
-
-      - name: Install Java
-        uses: actions/setup-java@v3
-        with:
-          distribution: 'temurin'
-          java-version: '17'
-
-      - name: Compile Java
-        run: javac Main.java
-
-      - name: Run Java
-        run: java Main
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello DevOps");
+    }
+}
